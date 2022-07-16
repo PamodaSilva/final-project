2321066a016519c54550348d9d24d628c4b8807e
pamodasilva99 <96543@fhss.sjp.ac.lk>
2022-07-16 14:18:42 +0530

    Edit Files.

--- Frontend/frontendcoffeehouse/src/App.js
+++ Frontend/frontendcoffeehouse/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+//import Navbar from './root/frontend/components/Navbar';
+//import Home from './root/frontend/Home/Home';
+//import Menu from './root/frontend/Menu/Menu';
+//import AboutUs from './root/frontend/AboutUs/AboutUs';
+//import Booking from './root/frontend/Booking/Booking';
+//import SignIn from './root/frontend/Login/SignIn/SignIn';
+//import SignUp from './root/frontend/Login/Register/SignUp';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar/>     
+        <Routes>
+          <Route path='/' />
+          
+        </Routes>
+      </Router>
+    </>
   );
 }
 
 export default App;
+
+/* 
+element={<Home/>}
+<Route path='/Menu' element={<Menu/>}/>
+          <Route path='/AboutUs' element={<AboutUs/>}/>
+          <Route path='/Booking' element={<Booking/>}/>
+          <Route path='/sign-in' element={<SignIn/>}/>
+          <Route path='/sign-up' element={<SignUp/>}/> */
